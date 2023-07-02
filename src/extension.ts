f2d270cb26187e7514422be5f6a5c0ab62426c62
ben <bengriepp@gmail.com>
2023-07-01 23:47:35 -0700

    add icon theme

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,22 @@ export function activate(context: vscode.ExtensionContext) {
     statusBar.show();
 
     context.subscriptions.push(statusBar);
+
+    context.subscriptions.push(vscode.commands.registerCommand('codexp.addXP', async () => {
+        let totalXP = context.globalState.get<number>('totalXP') || 0;
+        
+        // Prompt the user to enter the amount of XP to add.
+        let xpToAdd = await vscode.window.showInputBox({ prompt: 'Enter the amount of XP to add:' });
+
+        if (xpToAdd !== undefined) {
+            totalXP += parseInt(xpToAdd);
+
+            // Store the updated XP back to globalState
+            context.globalState.update('totalXP', totalXP);
+
+            updateStatusBar(totalXP);
+        }
+    }));
     
     lastSaveTime = new Date();
 
@@ -41,17 +57,18 @@ export function activate(context: vscode.ExtensionContext) {
         let totalXP = context.globalState.get<number>('totalXP') || 0;
         let level = calculateLevel(totalXP);
         let xpNeededForNextLevel = calculateXPNeededForNextLevel(level);
-        vscode.window.showInformationMessage(`You are level ${level}. Total XP: ${totalXP}. XP needed for next level: ${xpNeededForNextLevel}`);
+        let xpProgressToNextLevel = calculateRemainingXP(totalXP);
+        vscode.window.showInformationMessage(`You are level ${level}. Total XP: ${xpProgressToNextLevel}. XP needed for next level: ${xpNeededForNextLevel}`);
     }));
 }
 
 function updateStatusBar(totalXP: number) {
     let level = calculateLevel(totalXP);
     let xpNeededForNextLevel = calculateXPNeededForNextLevel(level);
-    let xpProgressToNextLevel = totalXP - calculateXPNeededForNextLevel(level - 1);
-    let progressBar = createProgressBar(xpProgressToNextLevel, xpNeededForNextLevel);
+    let xpProgressToNextLevel = totalXP - calculateXPNeededForNextLevel(level);
+    let progressBar = createProgressBar(xpProgressToNextLevel, xpNeededForNextLevel, 10);
 
-    statusBar.text = `Level ${level}: ${progressBar}`;
+    statusBar.text = `Lvl ${level}: ${progressBar}`;
 }
 
 function deactivate() {}
@@ -75,9 +92,43 @@ function calculateXPNeededForNextLevel(level: number): number {
     return 1000 + (level * 100);
 }
 
-function createProgressBar(current: number, total: number, barSize: number = 20): string {
+function calculateRemainingXP(totalXP: number): number {
+    let currentLevel = calculateLevel(totalXP);
+    let nextLevelXP = calculateXPNeededForNextLevel(currentLevel + 1);
+
+    return nextLevelXP - totalXP;
+}
+
+function createProgressBar(current: number, total: number, barSize: number = 10): string {
     current = Math.max(current, 0); // ensure current is at least 0
+    current = Math.min(current, total); // ensure current does not exceed total
+
     let percentage = current / total;
     let progress = Math.round(percentage * barSize);
-    return '[' + '='.repeat(progress) + '-'.repeat(barSize - progress) + ']';
+
+    // An array of the icon names in the order that they should appear.
+    let icons = ['progress-3', 'progress-4', 'progress-5', 'progress-6', 'progress-7', 'progress-8', 'progress-9', 'progress-10', 'progress-11', 'progress-12'];
+
+    // Initialize the progress bar with the beginning icon.
+    let progressBar = '$(progress-beginning)';
+
+    // Add the current progress icons.
+    for (let i = 0; i < progress; i++) {
+        progressBar += `$(progress-15)`;
+    }
+
+    // If progress is less than barSize, add the appropriate icon from the icons array.
+    if (progress < barSize) {
+        progressBar += `$(${icons[Math.floor((percentage % 0.1) * 10)]})`;
+    }
+
+    // Add the remaining empty space.
+    for (let i = progress + 1; i < barSize; i++) {
+        progressBar += '$(progress-1)';
+    }
+
+    // Add the end icon.
+    progressBar += '$(progress-end-1)';
+
+    return progressBar;
 }
